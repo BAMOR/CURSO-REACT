e9ceebf082847d68a875d4995708556595fb1fba
paulo <paulojosueb58@gmail.com>
2025-10-09 19:34:12 -0600

    Primer commit: Estructura inicial del proyecto

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,14 @@
+
+
+
+let firstName = 'Fernando';
+const lastName = 'Herrera';
+
+let number = 5;
+number = 3;
+
+const containsLetterH = lastName.includes('H');
+
+
+
+console.log({firstName,lastName, containsLetterH, number});
---
+++ 01-reforzamiento/src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+import './bases/03-object-literal'
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  <h1>Hola mundo</h1>
+    
+  </div>
+`
+
+
