713abeb436cb3f43b28013b0d0918cebfd04c2d2
paulo <paulojosueb58@gmail.com>
2025-11-13 16:50:20 -0600

    04-hooks

---
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,21 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
+
+export const SearchPage = () => {
+  return (
+    <>
+    <CustomJumbotron
+           title="Busqueda de SuperHeroes"
+           description="Descubre, explora y administra super heroes y villanos"
+           />
+
+    <HeroStats/>
+
+    <SearchControls/>
+    </>
+  )
+}
+
+
+export default SearchPage;
