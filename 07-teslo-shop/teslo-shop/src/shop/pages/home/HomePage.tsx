7e695be70455d3271f50ef077cc27132050be925
paulo <paulojosueb58@gmail.com>
2025-11-19 12:00:08 -0600

    diseno de homepage y paginacion

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,7 +1,19 @@
+import { CustomPagination } from "@/components/Custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJubotron } from "@/shop/components/CustomJubotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+
 
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+    <CustomJubotron title="Todos los productos"/>
+
+    <ProductsGrid products={products}/>
+    <CustomPagination totalPages={7}/>
+
+
+      </>
   )
 }
